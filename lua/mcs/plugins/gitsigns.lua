8b14ed3309d497b205aaac51e420512e8184b9d0
mcsymiv <s.mcsymiv@gmail.com>
2023-04-09 21:07:14 +0300

    add git plugins

---
+++ lua/mcs/plugins/gitsigns.lua
@@ -0,0 +1,15 @@
+local M = {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    -- See `:help gitsigns.txt`
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+  },
+}
+
+return M
