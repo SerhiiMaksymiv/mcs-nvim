3d2c0ebc7e5a7ecc8e8d1b4a80f89ffff3b01744
mcsymiv <s.mcsymiv@gmail.com>
2023-04-09 21:35:55 +0300

    add js to treesitter

--- lua/mcs/plugins/treesitter.lua
+++ lua/mcs/plugins/treesitter.lua
@@ -8,7 +8,7 @@ local M = {
     pcall(require('nvim-treesitter.install').update { with_sync = true })
     require('nvim-treesitter.configs').setup {
       -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = { 'go', 'lua', 'typescript', 'vimdoc', 'vim' },
+      ensure_installed = { 'go', 'lua', 'javascript', 'typescript', 'vimdoc', 'vim' },
 
       -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
       auto_install = false,
