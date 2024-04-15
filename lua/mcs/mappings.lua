4b72460b21253d739059f427e44d9e62c089b1ec
mcsymiv <s.mcsymiv@gmail.com>
2024-04-15 16:33:42 +0300

    edit telescope cur buf finder

--- lua/mcs/mappings.lua
+++ lua/mcs/mappings.lua
@@ -7,11 +7,7 @@ map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 map('n', '<leader>x', vim.cmd.Ex)
 
 vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
+  require('telescope.builtin').current_buffer_fuzzy_find()
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
