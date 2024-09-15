52a1c774bc470331374e47c0a0e85d0df6e6287d
mcsymiv <mcs@mcsymivs-MacBook-Pro.local>
2024-09-15 22:38:38 +0300

    add harpoon

--- lua/mcs/mappings.lua
+++ lua/mcs/mappings.lua
@@ -17,3 +17,22 @@ vim.keymap.set('n', '<c-u>', '<c-u>zz', { noremap = true })
 vim.keymap.set('n', '<leader>ee', vim.diagnostic.goto_next, { noremap = true })
 
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<C-a>", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+-- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+-- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+-- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+-- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-h>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():next() end)
---
+++ lua/mcs/plugins/harpoon.lua
@@ -0,0 +1,8 @@
+-- look for keys in mappings.lua
+local M = {
+  'ThePrimeagen/harpoon',
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" }
+}
+
+return M
