98ed83cc34a4509d7e34fd901cf49d30aa1d1bcd
mcsymiv <mcs@mcsymivs-MacBook-Pro.local>
2025-09-19 18:51:53 +0300

    upd lspconfig 0.11

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+--[[
 local M = {
   'neovim/nvim-lspconfig',
   dependencies = {
@@ -118,3 +119,36 @@ local M = {
 }
 
 return M
+]]
+
+return {
+    -- LuaLS configuration for vim
+    {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
+    -- Lsp notifications
+    {
+        "j-hui/fidget.nvim",
+        event = "BufReadPre",
+        opts = {},
+    },
+    -- Lsp installer and loader
+    {
+        "williamboman/mason.nvim",
+        event = "BufReadPre",
+        config = true,
+    },
+    -- Token Illuminate
+    {
+        "RRethy/vim-illuminate",
+        event = "BufReadPre",
+    },
+}
