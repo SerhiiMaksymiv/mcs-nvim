153c05cfaacf4b15e0fd9730354bf719ba4e114e
mcsymiv <s.mcsymiv@gmail.com>
2025-10-18 13:58:27 +0600

    add go lsp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,21 +6,21 @@ local M = {
     snippets = { preset = 'luasnip' },
     keymap = {
       preset = 'enter',
-    --   ['<Tab>'] = {
-    --     function(cmp)
-    --       if cmp.snippet_active() then
-    --         return cmp.accept()
-    --       else
-    --         return cmp.select_and_accept()
-    --       end
-    --     end,
-    --     -- 'snippet_forward',
-    --     'fallback',
-    --   },
-    --   ['<S-Tab>'] = {
-    --     -- 'snippet_backward',
-    --     'fallback',
-    --   },
+      ['<Tab>'] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        -- 'snippet_forward',
+        'fallback',
+      },
+      ['<S-Tab>'] = {
+        -- 'snippet_backward',
+        'fallback',
+      },
     },
 
     appearance = {
